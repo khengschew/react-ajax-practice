a098c8a6ff715cfb1c39fab70ccea886b52c2b65
khengschew <khengc@gmail.com>
2019-01-21 17:47:01 -0800

    Add comments for future reference on Fetch API

--- src/sendMessage.js
+++ src/sendMessage.js
@@ -14,6 +14,8 @@ var sendMessage = (message, callback) => {
   // });
 
   // Fetch method
+  // Note: since our response returns text, we cast to text using .text()
+  // For more data types to convert to, reference MDN: https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
   fetch('http://ec2-13-57-25-101.us-west-1.compute.amazonaws.com:3000/api/hrsf111/greeting', {
     method: 'POST',
     body: JSON.stringify(message),
@@ -25,6 +27,7 @@ var sendMessage = (message, callback) => {
   // .then(res => console.log(res))
   .then(data => callback(data))
   .catch(error => console.error('Failed to send message', error));
+
 }
 
 export default sendMessage;
