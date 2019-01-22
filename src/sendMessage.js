071c61c83277085b541b0fbd2269583ca4dc7c50
khengschew <khengc@gmail.com>
2019-01-21 17:44:54 -0800

    Update sendMessage to use Fetch API instead of jQuery ajax

--- src/sendMessage.js
+++ src/sendMessage.js
@@ -1,16 +1,30 @@
 import $ from 'jquery';
 
 var sendMessage = (message, callback) => {
-  $.ajax({
-    url: 'http://ec2-13-57-25-101.us-west-1.compute.amazonaws.com:3000/api/hrsf111/greeting',
-    type: 'POST',
-    data: JSON.stringify(message),
-    contentType: 'application/json',
-    success: (data) => (callback(data)),
-    error: function(error) {
-      console.error('Failed to send message', error);
+  // Ajax method
+  // $.ajax({
+  //   url: 'http://ec2-13-57-25-101.us-west-1.compute.amazonaws.com:3000/api/hrsf111/greeting',
+  //   type: 'POST',
+  //   data: JSON.stringify(message),
+  //   contentType: 'application/json',
+  //   success: (data) => (callback(data)),
+  //   error: function(error) {
+  //     console.error('Failed to send message', error);
+  //   }
+  // });
+
+  // Fetch method
+  fetch('http://ec2-13-57-25-101.us-west-1.compute.amazonaws.com:3000/api/hrsf111/greeting', {
+    method: 'POST',
+    body: JSON.stringify(message),
+    headers: {
+      'Content-Type': 'application/json'
     }
-  });
+  })
+  .then(res => res.text())
+  // .then(res => console.log(res))
+  .then(data => callback(data))
+  .catch(error => console.error('Failed to send message', error));
 }
 
 export default sendMessage;
